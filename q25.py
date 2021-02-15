248278e6f53a380252f6eef59a70a54913c45065
suhail <78995086+suhail2021@users.noreply.github.com>
2021-02-15 08:54:29 +0530

    Add files via upload

---
+++ q25.py
@@ -0,0 +1,13 @@
+customer_num =5
+
+invoice_num =1212
+
+print("Invoice No(s):")
+
+while(customer_num >0):
+
+     print("INV -", invoice_num)
+
+     invoice_num = invoice_num +3
+
+     customer_num = customer_num -1
