71105a8526a61bb1a808f42505416af85f978850
suhail <78995086+suhail2021@users.noreply.github.com>
2021-02-15 08:39:46 +0530

    Add files via upload

---
+++ q6.py
@@ -0,0 +1,10 @@
+n=int(input("enter the number:"))
+for i in range(0,n):
+	for j in range(0,i+1):
+		print("*",end=" ")
+	print()	
+
+for i in range(n,-1,-1):
+	for j in range(0,i+1):
+	 	print("*",end=" ")
+	print()		
