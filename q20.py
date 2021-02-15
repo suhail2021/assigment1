841a9bf4bd5c37f39cb9f72001981140de1c449e
suhail <78995086+suhail2021@users.noreply.github.com>
2021-02-15 08:53:07 +0530

    Add files via upload

---
+++ q20.py
@@ -0,0 +1,4 @@
+print("addition of two complex numbers:",(4+3j)+(3-7j))
+print("subtraction of two complex numbers:",(4+3j)-(3-7j))
+print("multipication of two complex numbers:",(4+3j)*(3_7j))
+print("division of two complex numbers:",(4+3j)/(3-7j))
