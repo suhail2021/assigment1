35be7c594d6739cced7519bf44d4ade6f489c12f
suhail <78995086+suhail2021@users.noreply.github.com>
2021-02-15 08:52:38 +0530

    Add files via upload

---
+++ q17.py
@@ -0,0 +1,10 @@
+# define punctuation
+punctuations='''!()-[]{};:<>./@$&*%
+my_str="hello!!!, he said ---and went."
+no_punct=""
+for char in my str:
+	if char not in punctuations:
+		no_punct =no_punct + char
+
+
+print(no_punct)		
